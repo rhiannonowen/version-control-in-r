33d511a6a3132feabf59b2ac8a0a7c3c25a3ba69
rhiannonowen <rhiannon_kate_owen@icloud.com>
2021-03-08 16:31:58 +0000

    R studio scores for 5 students

---
+++ student-scores.R
@@ -0,0 +1,11 @@
+# Create a vector called scores
+Scores<- c(65,68,70,70,71)
+# Create a character vector called Names
+Names <- c("Tayo", "Chance", "Seyi", "Louis", "Phil")
+length(Names)
+# Create a character vector called Gender
+Gender <- c("F", "M", "F", "M", "M")
+length(Gender)
+# Use the data.frame function to create a data frame
+stuMarks <- data.frame(Names, Scores, Gender)
+stuMarks
